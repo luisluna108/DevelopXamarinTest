ebcdd58a812c61fb5be644ea1dcc3b343f629c73
luisluna108 <55267010+luisluna108@users.noreply.github.com>
2019-09-16 21:16:02 -0600

    Create Services Api

---
+++ DevelopXamarinTest.Common/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevelopXamarinTest.Common.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public Decimal Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public DateTime PublishdOn { get; set; }
+    }
+}
---
+++ DevelopXamarinTestBackend/Models/LocalDataContext.cs
@@ -0,0 +1,9 @@
+﻿using DevelopXamarinTest.Domain.Models;
+
+namespace DevelopXamarinTestBackend.Models
+{
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<DevelopXamarinTest.Common.Models.Product> Products { get; set; }
+    }
+}
